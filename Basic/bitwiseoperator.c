8b035d27b47697452cf503c023a8a6733e8fbadb
AႦԋιʂԋҽƙ ƙυɱαɾ ραƚҽʅ <159283155+abhishek4901@users.noreply.github.com>
2025-04-05 06:02:12 +0530

    bitwise

---
+++ Basic/bitwiseoperator.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+    int a=1, b=2
+
+    ;
+    printf("a&b=%d\n",a&b);// bitwise and operator
+    printf("a|b=%d\n",a|b);// bitwise or operator
+    printf("!a=%d\n",!a); //not operator
+    // printf("a>0||b>5=%d",a||b); //or operator
+    // printf("a&&b=%d\n",a&&b);and operator
+    // if(a>0&&b>0){
+    // printf("a=%d b=%d\n",a,b);
+
+    // }
+    // else {
+
+    // printf("this a and b value is zero %d\n",a<b);
+   
+    // }
+return 0;
+}

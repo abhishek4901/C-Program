a4ae6d9603c2c806b1b7c4b467fce511db8efef1
AႦԋιʂԋҽƙ ƙυɱαɾ ραƚҽʅ <159283155+abhishek4901@users.noreply.github.com>
2025-04-24 23:51:39 +0530

    Basic

--- Basic/fact.c
+++ Basic/fact.c
@@ -1,33 +1,20 @@
 #include <stdio.h>
+
 // Function to calculate factorial using recursion
-int factorial(int n) {
+long long factorial(int n) {
     if (n == 0 )  // Base case
         return 1;
     else
         return n * factorial(n - 1); // Recursive call
 }
+
 int main() {
-    int n;//variabme different no matter
+    int num;
     printf("Enter a number: ");
-    scanf("%d", &n);
-     if ( n>0)
-     printf("Factorial = %d\n", factorial(n)); // Function call
- else
-     printf("Factorial is not defined for negative numbers.\n");
+    scanf("%d", &num);
+
+
+        printf("Factorial = %d \n", factorial(num));
+
     return 0;
 }
-
-
-/*
-#include<stdio.h>  
-int main()    
-{    
- int i,fact=1,number;    
- printf("Enter a number: ");    
-  scanf("%d",&number);    
-    for(i=1;i<=number;i++){    
-      fact=fact*i;    
-  }    
-  printf("Factorial of is: %d",fact);    
-return 0;  
-}   */ 

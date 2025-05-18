bccb8ed54a2e2c96850dfd3c3a0fe5487a7e9cf7
AႦԋιʂԋҽƙ ƙυɱαɾ ραƚҽʅ <159283155+abhishek4901@users.noreply.github.com>
2025-05-18 22:48:13 +0530

    AlignGivenZeroOneSide.c

--- DSA/Array/AlignGivenZeroOneSide.c
+++ DSA/Array/AlignGivenZeroOneSide.c
@@ -7,7 +7,7 @@ int main (){
         printf("%d ",arr[i]);
     for(i=0;i<size;i++){
         for(j=i+1;j<size;j++){
-            if(arr[i]>arr[j]){
+            if(arr[i]>arr[j]){ 
                 temp=arr[i];
                 arr[i]=arr[j];
                 arr[j]=temp;

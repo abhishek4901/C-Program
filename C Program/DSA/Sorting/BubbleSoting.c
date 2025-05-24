d1490ffefcff7e0eaf881d080d638f01785e7fe7
AႦԋιʂԋҽƙ ƙυɱαɾ ραƚҽʅ <159283155+abhishek4901@users.noreply.github.com>
2025-05-24 23:18:32 +0530

    BubbleSoting.c

--- C Program/DSA/Sorting/BubbleSoting.c
+++ C Program/DSA/Sorting/BubbleSoting.c
@@ -2,7 +2,7 @@
 #define MAX 100
 int main() 
 { 
-    int i, j, n, temp;  
+    int i, j, n, temp;   
     int arr[MAX];
     printf("Enter the number of elements you want to sort: ");
     scanf("%d", &n);

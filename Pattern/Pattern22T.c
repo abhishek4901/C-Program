5c537b74106f7b19ca22a97f942b3012b69f5c69
AႦԋιʂԋҽƙ ƙυɱαɾ ραƚҽʅ <159283155+abhishek4901@users.noreply.github.com>
2025-07-07 22:12:48 +0530

    Pattern22T.c

--- Pattern/Pattern22T.c
+++ Pattern/Pattern22T.c
@@ -4,7 +4,7 @@
         printf("Enter the number of rows: ");   
         scanf("%d",&n);
    
-     for(i=1;i<=n;i++){
+     for(i=1;i<=n;i++){      
         for(k=1;k<=n-i;k++){
             printf(" "); 
         }
